9a2e66ee2dd17a58de105402508b1c2228264f80
livingstonex <livooz71@gmail.com>
2021-12-15 09:45:07 +0100

    chore: added correct route for the rspec test

--- spec/requests/events_spec.rb
+++ spec/requests/events_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'Events API', type: :request do
     it 'returns all events' do
-        get '/events'
+        get '/api/v1/events'
 
         expect(response).to have_http_status(:unauthorized)
     end
